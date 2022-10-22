a34ec57ebb750114e234a21810f68240f9df3e79
Aliana K <alianafatima2013@gmail.com>
2022-10-22 12:42:56 +0530

    Added Admin interface for managing db

--- backend/characters/admin.py
+++ backend/characters/admin.py
@@ -1,7 +1,35 @@
 from django.contrib import admin
 from .models import *
 
-class companionAmin(admin.ModelAdmin):
+class EpisodeAdmin(admin.ModelAdmin):
+    list_display = ["title","original_air_date","viewer_rating","serial"]
+
+class SerialAdmin(admin.ModelAdmin):
+    list_display = ["title","season","serial_no","story"]
+
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = ["season_no","start_yr","end_yr"]
+
+class CompanionAdmin(admin.ModelAdmin):
     list_display = ["name","nickname","image"]
 
-admin.site.register(Companion, companionAmin)
+class VillainAdmin(admin.ModelAdmin):
+    list_display = ["name","nickname","image","description"]
+
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ["number","fav_clothing","image"]
+
+class AlienRaceAdmin(admin.ModelAdmin):
+    list_display = ["name","image","description"]
+
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ['name','gender']
+
+admin.site.register(Serial,SerialAdmin)
+admin.site.register(Episode, EpisodeAdmin)
+admin.site.register(Season, SeasonAdmin)
+admin.site.register(Companion, CompanionAdmin)
+admin.site.register(Villain, VillainAdmin)
+admin.site.register(Doctor, DoctorAdmin)
+admin.site.register(AlienRace, AlienRaceAdmin)
+admin.site.register(Actor, ActorAdmin)
