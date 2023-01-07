35e71985b627eb9797e4be4537989fabb99cbfa2
Aliana K <alianafatima2013@gmail.com>
2023-01-07 12:55:24 +0530

    Adding code for deployment

--- backend/manage.py
+++ backend/manage.py
@@ -8,6 +8,8 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home.settings')
     try:
+        from django.core.management.commands.runserver import Command as runserver
+        runserver.default_port = "3000"
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
