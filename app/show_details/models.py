bc60af3de240ba3b47399c20445c6d8d1983e2cc
Aliana K <alianafatima2013@gmail.com>
2024-10-13 01:17:50 +0530

    removal of unecessary comment

--- app/show_details/models.py
+++ app/show_details/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-# Create your models here.
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
@@ -29,13 +27,14 @@ class Serial(models.Model):
     serial_no = models.PositiveSmallIntegerField()
     story = models.TextField()
 
+    class Meta:
+        db_table = 'characters_serial'
+
     def __str__(self):
         if self.season.season_no<11200:
             return "Season {} Serial {}: {}".format(self.pk,self.season.season_no, self.serial_no, self.title)
         return "Season {} Series {}: {}".format(self.season.season_no-11200, self.serial_no, self.title)
 
-    class Meta:
-        db_table = 'characters_serial'
 
 
 class Episode(models.Model):
@@ -53,12 +52,12 @@ class Episode(models.Model):
         ],max_digits=2, decimal_places=1)
     serial = models.ForeignKey(Serial, null=True, blank=True, on_delete=models.CASCADE)
 
+    class Meta:
+        db_table = 'characters_episode'
 
     def __str__(self):
         return self.title
 
-    class Meta:
-        db_table = 'characters_episode'
 
 class Actor(models.Model):
     name = models.CharField(max_length=70)
